f05c376af94f3054434e250effda3bd4dc35cbda
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-06-24 22:08:08 +0530

    Using python library

---
+++ ultramain.py
@@ -0,0 +1,7 @@
+import instaloader
+import os
+mod = instaloader.Instaloader()
+a = input("Enter the profile name: ")
+mod.download_profile(a, profile_pic_only=True)
+
+os.system(f" instaloader (a) --profile-pic-only")
